72effdab4bb473a272a16c2c032536361d98aa9b
Ankeet Maini <ankeet.maini@gmail.com>
2019-10-20 19:22:56 +0530

    Not quite.

--- src/Root.tsx
+++ src/Root.tsx
@@ -2,8 +2,6 @@ import React, { useRef } from "react";
 import {
   StyleSheet,
   SafeAreaView,
-  ScrollView,
-  Dimensions,
   Animated,
   PanResponder,
   View
@@ -11,13 +9,23 @@ import {
 import Card from "./Card";
 
 const TOTAL_CARDS = 15;
+const DRAG_AMOUNT = 100;
+const CARDS_ARRAY = Array.from({ length: TOTAL_CARDS });
+
 export default function Root() {
   const animationValue = useRef(new Animated.Value(0)).current;
+  let startPanY = useRef(0).current;
+  let totalPanY = useRef(0).current;
   const panResponder = useRef(
     PanResponder.create({
       onMoveShouldSetPanResponder: () => true,
-      onPanResponderMove: (_, { dy }) => {
-        console.log(dy);
+      onPanResponderGrant: (_, { y0 }) => {
+        startPanY = y0;
+      },
+      onPanResponderMove: (_, { moveY }) => {
+        totalPanY += moveY - startPanY;
+        startPanY = moveY;
+        animationValue.setValue(totalPanY);
       }
     })
   ).current;
@@ -30,18 +38,33 @@ export default function Root() {
         {...panResponder.panHandlers}
         style={[StyleSheet.absoluteFill, { backgroundColor: "red" }]}
       >
-        {Array.from({ length: TOTAL_CARDS }).map((_, i) => {
-          const translateValue = animationValue.interpolate({
-            inputRange: [0, 1],
-            outputRange: [-i * 2, -50 + i * 10],
-            extrapolateLeft: "clamp"
+        {CARDS_ARRAY.map((_, i) => {
+          const variableAmount = TOTAL_CARDS - i - 1 || TOTAL_CARDS - 1;
+          console.log({ variableAmount, i });
+          const inputRange = [
+            0,
+            variableAmount * DRAG_AMOUNT,
+            variableAmount * DRAG_AMOUNT + DRAG_AMOUNT
+          ];
+          const outputRange = [
+            0,
+            0,
+            variableAmount * DRAG_AMOUNT + DRAG_AMOUNT
+          ];
+
+          const translateY = animationValue.interpolate({
+            inputRange,
+            outputRange,
+            extrapolate: "clamp"
           });
+          // console.log(values, animationValue);
           return (
             <Animated.View
               key={i}
               style={[
-                { transform: [{ translateY: translateValue }] },
-                StyleSheet.absoluteFill
+                { transform: [{ translateY }] },
+                StyleSheet.absoluteFill,
+                {}
               ]}
             >
               <Card index={i} />
